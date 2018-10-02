80143c8a9c538f8fc5d4355a50adedc9c1eae692
Camilo José Mezú Mina <camilo272001@gmail.com>
2018-10-01 23:02:22 -0500

    Archivos base

---
+++ Edx/01-punteros.cpp
@@ -0,0 +1,19 @@
+ // #include "stdafx.h"
+    #include <iostream>
+    
+    using namespace std;
+    
+    int main()
+    {
+    
+    int num = 3;
+    int * pNum = & num;
+    cout << pNum << endl;
+    cout << * pNum << endl;
+
+    * pNum = 45;
+    cout << * pNum << endl;
+    //cout << num << endl;
+
+        return 0;
+    }
---
+++ Edx/02-Referencias.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    int num = 3;
+    int &refNum = num;
+
+    cout << "num contains " << num << endl;
+    cout << "refNum contains " << refNum << endl;
+
+    refNum++;    // increment refNum by 1
+
+    cout << "num contains " << num << endl;
+    cout << "refNum contains " << refNum << endl;
+    cout << "refNum is located at " << &refNum << " and num is located at " << &num << endl;
+
+
+return 0;
+}
