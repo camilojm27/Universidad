8922504ae85689c036cfc4a965f672ae3d3f87ec
Camilo José Mezú Mina <camilo272001@gmail.com>
2019-02-20 10:05:22 -0500

    Se ha terminado y rediseñado completamente el taller 3-laberinto

--- IPOO/taller-3/Laberinto.cpp
+++ IPOO/taller-3/Laberinto.cpp
@@ -1,10 +1,17 @@
 /*
   Archivo: Laberinto.cpp
-  Autor: Ángel García Baños
-  Email: angel.garcia@correounivalle.edu.co
+  Basado en el codigo de Angel Garcia Bolaños angel.garcia@correounivalle.edu.co
+  Autores:
+           
+  Santiago Martinez Mesa (1823107-3743)
+  Camilo Mezu Mina (1824313)
+           
+  Email: camilo.mezu@correounivalle.edu.co
+  santiago.mesa@correounivalle.edu.co
+  
   Fecha creación: 2018-09-17
-  Fecha última modificación: 2018-09-24
-  Versión: 0.1
+  Fecha última modificación: 2019-02-19
+  Versión: 0.4
   Licencia: GPL
 */
 
@@ -18,134 +25,136 @@ Laberinto::Laberinto(int numeroFilas, int numeroColumnas, int numeroTesoros, int
   this->numeroGnomos = numeroGnomos;
   this->numeroDragones = numeroDragones;
   this->porcentajeCasillasVacias = porcentajeCasillasVacias;
-  tablero = new int*[numeroFilas];
-  for(int fila=0; fila<numeroFilas; fila++)
+  tablero = new int *[numeroFilas];
+  for (int fila = 0; fila < numeroFilas; fila++)
   {
     tablero[fila] = new int[numeroColumnas];
-    for(int columna=0; columna<numeroColumnas; columna++)
-      tablero[fila][columna] = 1;  // Llena todo el laberinto con paredes
+    for (int columna = 0; columna < numeroColumnas; columna++)
+      tablero[fila][columna] = 1; // Llena todo el laberinto con paredes
   }
   casillasVacias = 0;
 
   filaEntrada = rand() % numeroFilas;
-  tablero[filaEntrada][0] = 5;  // Pone la entrada
+  tablero[filaEntrada][0] = 5; // Pone la entrada
   filaSalida = rand() % numeroFilas;
-  tablero[filaSalida][numeroColumnas-1] = 6;  // Pone la salida
+  tablero[filaSalida][numeroColumnas - 1] = 6; // Pone la salida
 }
-
 
 Laberinto::~Laberinto()
 {
-  for(int fila=0; fila<numeroFilas; fila++)
+  for (int fila = 0; fila < numeroFilas; fila++)
     delete tablero[fila];
-  delete [] tablero;
+  delete[] tablero;
 }
-
 
 void Laberinto::fabricarCamino()
 {
   // Taller 4
-  
+
   // Hacer el camino de salida:
-  casillasVacias += trazaLineaRecta(filaEntrada, 0, filaSalida, numeroColumnas-1);
+  casillasVacias += trazaLineaRecta(filaEntrada, 0, filaSalida, numeroColumnas - 1);
   // Hacer caminos al azar:
- 
+  //for(int veces=0; veces <=6; veces++)
+  {
+    for (int fila = 1; fila < numeroFilas - 1; fila++)
+    {
+      for (int columna = 1; columna < numeroColumnas - 1; columna++)
+      {
+        if (tablero[fila][columna] == 1)
+        {
+          int random = rand() % numeroFilas;
+          // randomColu = rand() % numeroColumnas-1;
+          tablero[random][columna] = 0;
+          tablero[random][columna - 1] = 0;
+          // tablero[random][columna-1] = 0;
+        }
+      }
+    }
+  }
+
+  //PRIMERA FUNCION PARA COLOCAR OBJETOS //
+  /*
   for (int veces=0; veces <= 15; veces++)
   {
-    
     int  radum = 2+rand()%(4-2);
     ponerEnCasillaVaciaAlAzar(radum);
-    
   }
-  
-  // Poner los Tesoros al azar
+  */
 
-  // Poner los Gnomos al azar
+  //SEGUNDA FUNCION PARA COLOCAR OBJETOS //
 
-  // Poner los Dragones al azar, cerca de la salida
+  //DEFINE EL NUMERO QUE REPRESENTA A CADA OBJETO EN EL TABLERO//
 
+  int Nomo = 2;
+  int Tesoro = 3;
+  int Dragon = 4;
+
+  //CREA EN UNA POSICION AL AZAR DETERMINADO OBJETO HASTA CUMPLIR LA CUOTA DE LA FUNCION CONSTRUCTORA//
+
+  for (int temp = 0; temp <= numeroTesoros; temp++)
+    ponerEnCasillaVaciaAlAzar(Nomo);
+
+  for (int temp = 0; temp <= numeroGnomos; temp++)
+    ponerEnCasillaVaciaAlAzar(Tesoro);
+
+  for (int temp = 0; temp <= numeroDragones; temp++)
+    ponerEnCasillaVaciaAlAzar(Dragon);
+
+  tablero[filaEntrada][0] = 5;
+  tablero[filaSalida][numeroColumnas - 1] = 6;
 }
-
 
 int Laberinto::trazaLineaRecta(int filaInicial, int columnaInicial, int filaFinal, int columnaFinal)
 {
+
+  //SEPARA LA FUNCION DE LA ECUACION DE LA RECTA EN PARTES PARA FACILITAR EL CALCULO DE
+  // LAS VARIABLES. //
+
+  float y = (filaFinal - filaInicial);
+  float x = (columnaFinal - columnaInicial);
+  float m1 = y / x;
+
+  //RETORNA SOLO VALORES ENTEROS, NO FUNCIONA ADECUADAMENTE //
+
+  //float m2 = (filaFinal - filaInicial) / (columnaFinal - columnaInicial);
+  //cout << pendiente << endl;
+  //cout << pendiente2 << endl
+
+  //VALOR TEMPORAL USADO COMO CONTADOR//
   int contarVacias = 0;
 
-  // Taller 4
-  
-   int m1 = (columnaFinal*1.0 - columnaInicial) / (filaFinal - filaInicial*1.0);
-  
-  
-  for(int fila=1; fila<numeroFilas-1;fila++)
+  for (int posx = 1; posx < columnaFinal; posx++)
   {
-    for(int columna=1; columna<numeroColumnas-1;columna++)
-    {
-      int m2 = (columnaFinal*1.0 - columna) / (filaFinal - fila*1.0);
-      
-      if(m1 == m2 || m1+1 == m2 ==  m1-1)
-      {
-        //cout << m1 << endl;
-        tablero[fila][columna] = 0;
-      }
-      else
-      {
-        if(m1==0)
-        {
-          tablero[fila][columna] = 0;
-        }
-        if(tablero[fila][columna] == 1)
-        {
-          int random = rand() % numeroFilas;
-          int randomColu = rand() % numeroColumnas-1;
-
-          tablero[random][columna] = 0;
-          
-          /*if(columna == numeroColumnas/2){
-            break;
-          }*/
-        }
-        else
-        {
-          //cout << m1 << endl;
-          //cout << m2 << endl;
-          tablero[fila][columna] = 1;
-          
-        }
-        
-      }
-      
-    }
+    int ecuacionRecta = (m1 * posx) + (m1 * columnaFinal * -1) + filaFinal;
+    tablero[ecuacionRecta][posx] = 0;
+    tablero[ecuacionRecta][posx + 1] = 0;
+    tablero[ecuacionRecta][posx - 1] = 0;
+    contarVacias++;
   }
-  
-  
+
   return contarVacias;
 }
 
-
-
-
-
 void Laberinto::imprimir()
 {
-  
-  for(int columna=0; columna<numeroColumnas+2; columna++)
+
+  for (int columna = 0; columna < numeroColumnas + 2; columna++)
     cout << "X";
   cout << endl;
-  for(int fila=0; fila<numeroFilas; fila++)
+  for (int fila = 0; fila < numeroFilas; fila++)
   {
     cout << "X";
-    for(int columna=0; columna<numeroColumnas; columna++)
-      if(tablero[fila][columna] == 0)
+    for (int columna = 0; columna < numeroColumnas; columna++)
+      if (tablero[fila][columna] == 0)
         cout << " ";
       else
         cout << tablero[fila][columna];
     cout << "X" << endl;
   }
-  for(int columna=0; columna<numeroColumnas+2; columna++)
+  for (int columna = 0; columna < numeroColumnas + 2; columna++)
     cout << "X";
   cout << endl;
 }
-    
 
 void Laberinto::buscarCasillaAlAzar(int &fila, int &columna, int contenido)
 {
@@ -153,10 +162,8 @@ void Laberinto::buscarCasillaAlAzar(int &fila, int &columna, int contenido)
   {
     fila = rand() % numeroFilas;
     columna = rand() % numeroColumnas;
-  }
-  while(tablero[fila][columna] != contenido);
+  } while (tablero[fila][columna] != contenido);
 }
-
 
 void Laberinto::ponerEnCasillaVaciaAlAzar(int contenido)
 {
@@ -164,7 +171,3 @@ void Laberinto::ponerEnCasillaVaciaAlAzar(int contenido)
   buscarCasillaAlAzar(fila, columna, 1);
   tablero[fila][columna] = contenido;
 }
-
-
-
-
